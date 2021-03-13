2faefe91915856a68e5b541fbeecac1227425fb7
imarkonis <imarkonis@gmail.com>
2021-03-13 18:53:02 +0100

    minor tweaks

--- code/01_data_prep.R
+++ code/01_data_prep.R
@@ -1,17 +1,12 @@
 # Reads csv and saves in .RDS
 library(data.table)
 
-temp <- data.table(read.csv("./data/ROS_data_reSubmission_Scenario.5.csv"))
-temp <- data.table(read.csv("ROS_data_reSubmission_Scenario.5.csv")) #alternative path
+temp <- data.table(read.csv("./data/raw/ROS_data_reSubmission_Scenario.5.csv"))
 
 temp[, Rain_Start := as.POSIXct(Rain_Start, format = "%m/%d/%Y %H:%M")]
 colnames(temp)[1] <- 'Event.Nr'
 saveRDS(temp, "./data/ROS_data.RDs")
-#alternative path
 
-temp <- data.table(read.csv(("./data/Summary_catchments_II.csv")))
-temp <- data.table(read.csv("", sep = ';')) #alternative path
-
-colnames(temp)[6] <- "catchment"
-saveRDS(temp, "./data/catchment_data.RDs")
-#alternative path
+#temp <- data.table(read.csv(("./data/Summary_catchments_II.csv")))
+#colnames(temp)[6] <- "catchment"
+#saveRDS(temp, "./data/catchment_data.RDs")
--- code/04_random_forests.R
+++ code/04_random_forests.R
@@ -1,15 +1,21 @@
-library(randomForest)
-library(parallel)
-library(foreach)
-library(DALEX)
+library(data.table)
 library(ggplot2)
+library(caret)
+library(doParallel)
+library(foreach)
+library(randomForest)
+library(DALEX)
 
-load('./data/som_4x4_main_4_vars_rev.rdata') 
+ncores <- detectCores() - 1
+cs <- makeCluster(ncores)
+registerDoParallel(cs)
+
+load('./results/som_4x4_4_vars.rdata') 
 my_palette <- colorRampPalette(c("#4C3F54", "#486824", "#F2C057", "#D13525"))
 n_groups <- 16 # number of groups with similar properties | each node is a group
 
-heatmap(cor(ros_events))
-iterations <- rep(10000, 1)
+heatmap(cor(ros_events[, 1:14]))
+iterations <- rep(10000, ncores)
 data_size <- nrow(ros_subset)
 train_size <- .8 * data_size
 
@@ -17,25 +23,38 @@ ros_subset$node <- as.factor(ros_som$unit.classif)
 ros_subset[, n_ros_group := .N, by = node] #number of ros events per hclust group
 
 colnames(ros_subset)
-train_data_maxQ <- ros_subset[1:train_size, c(2:3, 5, 7:10, 13:16)] 
-validation_data_maxQ <- ros_subset[(train_size + 1):data_size, c(2:3, 5, 7, 9:10, 13:16)]
+train_data_maxQ <- ros_subset[1:train_size, c(2:3, 5:7, 11, 13:15)] 
+validation_data_maxQ <- ros_subset[(train_size + 1):data_size, c(2:3, 5:7, 11, 13:15)]
 
+#decide which is best
+maxQ_rf_model <- train(maxQ ~ ., data = train_data_maxQ, method = 'rf') 
 maxQ_rf_model <- foreach(ntree = iterations, .combine = randomForest::combine, 
                      .multicombine = TRUE, .packages = 'randomForest') %dopar% {
                        randomForest(maxQ ~ ., data = train_data_maxQ, ntree = ntree)
                      }
+
 varImpPlot(maxQ_rf_model)
 var_imp <- data.frame(importance(maxQ_rf_model, type = 2))
 var_imp$Variables <- row.names(var_imp)  
 var_imp[order(var_imp$IncNodePurity, decreasing = T), ]
 
 explain_maxQ_rf <- explain(model = maxQ_rf_model,  
-                             data = train_data_maxQ[, -8],
+                             data = train_data_maxQ[, -6],
                              y = train_data_maxQ$maxQ, 
                              label = "Random Forest")
+maxQ_rf_perform <- model_performance(explain_maxQ_rf)
+
+vip_maxQ_rf <- feature_importance(explain_maxQ_rf, n_sample = 8000,
+                              loss_function = loss_root_mean_square) 
+
+plot(vip_maxQ_rf, max_vars = 10) +
+  ggtitle("Permutation variable importance", 
+          "Average variable importance based on 8,000 permutations") +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5))
 
 bd_maxQ_rf <- predict_parts(explainer = explain_maxQ_rf,
-                            new_observation = validation_data[1, ],
+                            new_observation = validation_data_maxQ[1, ],
                             type = "break_down")
 plot(bd_maxQ_rf) 
 
