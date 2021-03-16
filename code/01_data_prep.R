44e588e8fde132d018321e56d46697d5f371c12d
Yannis Markonis <imarkonis@gmail.com>
2021-03-16 09:57:44 +0100

    small fixes

--- code/01_data_prep.R
+++ code/01_data_prep.R
@@ -2,11 +2,8 @@
 library(data.table)
 
 temp <- data.table(read.csv("./data/raw/ROS_data_reSubmission_Scenario.5.csv"))
-
 temp[, Rain_Start := as.POSIXct(Rain_Start, format = "%m/%d/%Y %H:%M")]
 colnames(temp)[1] <- 'Event.Nr'
+
 saveRDS(temp, "./data/ROS_data.RDs")
 
-#temp <- data.table(read.csv(("./data/Summary_catchments_II.csv")))
-#colnames(temp)[6] <- "catchment"
-#saveRDS(temp, "./data/catchment_data.RDs")
